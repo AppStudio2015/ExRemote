5beeafb8f21c364af618c40ec850abf7fb88a13a
lihaoEmily <lihao@mapbar.com>
2016-05-10 17:45:02 +0800

    Remote界面

--- ExRemote/ViewController.swift
+++ ExRemote/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var remoteView = RemoteView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        remoteView = RemoteView.init(frame: CGRect(x: 0, y: 0, width: UIScreen .mainScreen().bounds.size.width, height: UIScreen .mainScreen().bounds.size.height))
+        self.view.addSubview(remoteView)
     }
 
     override func didReceiveMemoryWarning() {
